33f1dea0403be79b867bfde661869d51e222d778
ew-aolcc <bizjay61@gmail.com>
2026-09-01 14:39:40 -0400

    Functions Assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    // var sent1 = "This is the beginning of the string";
+    // var sent2 = " and this is the end of the string";
+    var sentence = "This is the beginning of the string";
+    sentence += " and this is the end of the string";
+
+    // Display the string in the <p> element
+    document.getElementById("Button_Text").innerHTML = sentence
+}
