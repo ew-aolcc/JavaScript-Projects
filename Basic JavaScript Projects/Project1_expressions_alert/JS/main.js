389c1091afc160c8fc41fe0b7cf830050f5cff59
ew-aolcc <bizjay61@gmail.com>
2026-09-01 14:22:27 -0400

    Basic Js Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,26 @@
+window.alert("Hello World!");
+
+// Expressions
+var num1 = 9;
+var num2 = 3;
+var sum = '9 + 3 = ' + (num1 + num2)
+// window.alert(sum)
+document.write(sum)
+
+// HTML break for new line
+document.write("<br>");
+
+// Escape Characters
+var singleQuote = "It\'s a beautiful day.";
+// document.write(singleQuote);
+
+// Concatenating Strings
+var sent1 = "This is the beginning of the string"
+var sent2 =  " and this is the end of the string"
+var sentence = sent1 + sent2;
+document.write(sentence);
+
+
+// Multiple variables
+var x = 5, y = 10, z = x + y;
+// document.write('The value of z is ' + z + '.')
